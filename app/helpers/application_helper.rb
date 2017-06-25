57d9b9f586a2d48cbb88fb6be8414dfe9a806aae
Cody Higgins <cody.higginsdev18@gmail.com>
2017-06-25 01:01:19 -0600

    added terms and privacy links, footer styles completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def link_fa_to(icon_name, text, link)
-    link_to content_tag(:i, text, :class => "fa fa-#{icon_name}"), link
+    link_to content_tag(:i, text, :class => "fa fa-#{icon_name} fa-icon-padding")
   end
 
   def alerts
