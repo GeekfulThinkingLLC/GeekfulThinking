d3fbc88a307d98e0909af0014c72ada6988f6bc8
Cody Higgins <cody.higginsdev18@gmail.com>
2017-06-24 18:00:11 -0600

    added devise, adjusted nav styles, implemented body content for social mission

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register'}
+
   resources :consultations
+
   get 'pages/home'
 
   get 'pages/contact'
